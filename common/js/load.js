0f15b1cb64e3a39cc56304978eac849d8e428abd
gw-k019g1048 <98067744+gw-k019g1048@users.noreply.github.com>
2022-01-20 10:53:14 +0900

    Add files via upload

---
+++ common/js/load.js
@@ -0,0 +1,13 @@
+setTimeout(function () {
+	var bg = document.getElementById("loader-bg");
+	bg.classList.add("clear");
+}, 1250);
+setTimeout(function () {
+	var bg = document.getElementById("loader-bg");
+	bg.classList.add("clear2");
+}, 1750);
+
+var wi = window.innerWidth;
+var he = window.innerHeight;
+if (he >= wi) {
+}
